00beff20a954dbe434bed3f83af5b4de3d85c81b
Lazlo <glebite@gmail.com>
2019-07-08 20:53:27 -0400

    cleaned up as per pylint

--- dcs5020l.py
+++ dcs5020l.py
@@ -1,114 +1,126 @@
-import requests
+"""
+dcs5020l
+"""
 import time
 import logging
+import requests
 
 logging.basicConfig(level=logging.DEBUG)
 
 
-class dcs5020l:
+class Dcs5020l:
+    """ Dcs5020l """
     def __init__(self):
+        """ init """
         self.url = ""
         self.user = ""
         self.password = ""
-        self.connectedState = 0
+        self.connected_state = 0
         logging.debug('Creating instance')
 
-    def connect(self, URL, user, password):
-        self.url = URL
+    def connect(self, url, user, password):
+        """ connect """
+        self.url = url
         self.user = user
         self.password = password
-        self.connectedState = 1
-        logging.debug('URL: %s user: %s password: %s' % (URL, user, password))
+        self.connected_state = 1
+        logging.debug('URL: %s user: %s password: %s', url, user, password)
 
     def disconnect(self):
-        self.connectedState = 0
+        """ disconnect """
+        self.connected_state = 0
 
-    def up(self, degrees=1):
+    def move_up(self, degrees=1):
+        """ up """
         delay = degrees / 20
-        logging.debug('up by ' + str(degrees))
-        sData = {'TiltSingleMoveDegree': str(degrees),
-                 'PanTiltSingleMove': '1'}
-        r = requests.post(self.url+'/pantiltcontrol.cgi',
-                          data=sData, auth=(self.user, self.password),
-                          verify=False)
+        logging.debug('up by %s', str(degrees))
+        string_data = {'TiltSingleMoveDegree': str(degrees),
+                       'PanTiltSingleMove': '1'}
+        result = requests.post(self.url+'/pantiltcontrol.cgi',
+                               data=string_data, auth=(self.user, self.password),
+                               verify=False)
         time.sleep(delay + 1)
-        return r
+        return result
 
-    def down(self, degrees=1):
+    def move_down(self, degrees=1):
+        """ down """
         delay = degrees / 20
-        logging.debug('down by ' + str(degrees))
-        sData = {'TiltSingleMoveDegree': str(degrees),
-                 'PanTiltSingleMove': '7'}
-        r = requests.post(self.url+'/pantiltcontrol.cgi',
-                          data=sData, auth=(self.user, self.password),
-                          verify=False)
+        logging.debug('down by %s', str(degrees))
+        string_data = {'TiltSingleMoveDegree': str(degrees),
+                       'PanTiltSingleMove': '7'}
+        result = requests.post(self.url+'/pantiltcontrol.cgi',
+                               data=string_data, auth=(self.user, self.password),
+                               verify=False)
         time.sleep(delay + 1)
-        return r
+        return result
 
-    def left(self, degrees=1):
+    def move_left(self, degrees=1):
+        """ left """
         delay = degrees / 15
-        logging.debug('left by ' + str(degrees))
-        sData = {'PanSingleMoveDegree': str(degrees), 'PanTiltSingleMove': '3'}
-        r = requests.post(self.url+'/pantiltcontrol.cgi',
-                          data=sData, auth=(self.user, self.password))
+        logging.debug('left by %s', str(degrees))
+        string_data = {'PanSingleMoveDegree': str(degrees), 'PanTiltSingleMove': '3'}
+        result = requests.post(self.url+'/pantiltcontrol.cgi',
+                               data=string_data, auth=(self.user, self.password))
         time.sleep(delay+1)
-        return r
+        return result
 
-    def right(self, degrees=1):
+    def move_right(self, degrees=1):
+        """ right """
         delay = degrees / 15
-        logging.debug('right by ' + str(degrees))
-        sData = {'PanSingleMoveDegree': str(degrees), 'PanTiltSingleMove': '5'}
-        r = requests.post(self.url+'/pantiltcontrol.cgi',
-                          data=sData, auth=(self.user, self.password),
-                          verify=False)
+        logging.debug('right by %s', str(degrees))
+        string_data = {'PanSingleMoveDegree': str(degrees), 'PanTiltSingleMove': '5'}
+        result = requests.post(self.url+'/pantiltcontrol.cgi',
+                               data=string_data, auth=(self.user, self.password),
+                               verify=False)
         time.sleep(delay+1)
-        return r
+        return result
 
-    def home(self):
+    def move_home(self):
+        """ home """
         logging.debug('return ptz to home')
-        p, t = self.getPosition()
-        logging.debug('before moving home: ' + str(p) + ' ' + str(t))
+        pan_pos, tilt_pos = self.get_position()
+        logging.debug('before moving home: %s %s', str(pan_pos), str(tilt_pos))
 
-        sData = {'PanTiltSingleMove': '4'}
-        r = requests.post(self.url+'/pantiltcontrol.cgi', data=sData,
-                          auth=(self.user, self.password), verify=False)
+        string_data = {'PanTiltSingleMove': '4'}
+        result = requests.post(self.url+'/pantiltcontrol.cgi', data=string_data,
+                               auth=(self.user, self.password), verify=False)
         time.sleep(15)
-        return r
+        return result
 
-    def getImage(self, fileName):
+    def get_image(self, file_name):
         """
         in: fileName
-        return: r - results of requests.get
+        return: result - results of requests.get
 
-        note: will potentially write a file 
+        note: will potentially write a file
         """
-        r = requests.get(self.url+'/image.jpg',
-                         auth=(self.user, self.password),
-                         stream=True, verify=False)
-        if r.status_code == 200:
-            with open(fileName, 'wb') as f:
-                for chunk in r.iter_content(1024):
-                    f.write(chunk)
-        return r
+        result = requests.get(self.url+'/image.jpg',
+                              auth=(self.user, self.password),
+                              stream=True, verify=False)
+        if result.status_code == 200:
+            with open(file_name, 'wb') as file_handle:
+                for chunk in result.iter_content(1024):
+                    file_handle.write(chunk)
+        return result
 
-    def getPosition(self):
+    def get_position(self):
         """
         return: pan position, tilt position
         """
 
-        r = requests.get(self.url+'/config/ptz_pos.cgi',
-                         auth=(self.user, self.password), verify=False)
-        for x in r.text.split("\n"):
-            if x.startswith('p'):
-                pan = int(x.split('=')[1])
-            if x.startswith('t'):
-                tilt = int(x.split('=')[1])
+        result = requests.get(self.url+'/config/ptz_pos.cgi',
+                              auth=(self.user, self.password), verify=False)
+        for item in result.text.split("\n"):
+            if item.startswith('p'):
+                pan = int(item.split('=')[1])
+            if item.startswith('t'):
+                tilt = int(item.split('=')[1])
         return pan, tilt
 
-    def daynight(self, mode):
+    def day_night(self, mode):
         """
-        return r - result of requests post
+        return result - result of requests post
         """
-        r = requests.post(self.url+'/nightmodecontrol.cgi?IRLed=' + str(mode),
-                          auth=(self.user, self.password))
-        return r
+        result = requests.post(self.url+'/nightmodecontrol.cgi?IRLed=' + str(mode),
+                               auth=(self.user, self.password))
+        return result
